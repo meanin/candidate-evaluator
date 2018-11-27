b0d48bd750489fc04334b7683233747a8d7c9658
plaumen <przemek.ciszak@gmail.com>
2018-11-27 19:59:01 +0100

    Add Name and Text properties to QuestionEntity

--- CandidateEvaluator.Data/Entities/QuestionEntity.cs
+++ CandidateEvaluator.Data/Entities/QuestionEntity.cs
@@ -4,5 +4,7 @@ namespace CandidateEvaluator.Data.Entities
 {
     public class QuestionEntity : TableEntity
     {
+        public string Name { get; set; }
+        public string Text { get; set; }
     }
 }
